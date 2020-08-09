713b0989de031d64362cbb1d8b669aa5722cae19
jingxi <jing.xi@hotmail.com>
2020-08-09 03:43:24 +0000

    ..

--- Program.cs
+++ Program.cs
@@ -1,38 +1,50 @@
-using System;
+﻿using System;
 
-namespace sc_presure
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace sc_presure {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
             //IPMI_Access.Foreloop();    
-         Console.WriteLine(   Ex());
- //IPMI_Access.Foreloop();
- }
-          static string Ex()
-    {
+            Console.WriteLine (LSEX ());
+            //IPMI_Access.Foreloop();
+        }
+        static string Ex () {
 
-string command = "ifconfig";//"write your command here";
-string result = "";
-using (System.Diagnostics.Process proc = new System.Diagnostics.Process())
-{
-    proc.StartInfo.FileName = "/bin/bash";
-    proc.StartInfo.Arguments = "-c \" " + command + @" -a | grep -i  'inet ' ";
-    proc.StartInfo.UseShellExecute = false;
-    proc.StartInfo.RedirectStandardOutput = true;
-    proc.StartInfo.RedirectStandardError = true;
-    proc.Start();
+            string command = "ifconfig"; //"write your command here";
+            string result = "";
+            using (System.Diagnostics.Process proc = new System.Diagnostics.Process ()) {
+                proc.StartInfo.FileName = "/bin/bash";
+                proc.StartInfo.Arguments = "-c \" " + command + @" -a | grep -i  'inet ' ";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.Start ();
 
-    result += proc.StandardOutput.ReadToEnd();
-    result += proc.StandardError.ReadToEnd();
+                result += proc.StandardOutput.ReadToEnd ();
+                result += proc.StandardError.ReadToEnd ();
 
-    proc.WaitForExit();
-}
-return result;
+                proc.WaitForExit ();
+            }
+            return result;
+        }
+        static string LSEX () {
 
+            string command = "ls"; //"write your command here";
+            string result = "";
+            using (System.Diagnostics.Process proc = new System.Diagnostics.Process ()) {
+                proc.StartInfo.FileName = "/bin/bash";
+                proc.StartInfo.Arguments = "-c \" " + command;// + @" -a | grep -i  'inet ' ";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.Start ();
 
-    }
+                result += proc.StandardOutput.ReadToEnd ();
+                result += proc.StandardError.ReadToEnd ();
+
+                proc.WaitForExit ();
+            }
+            return result;
+        }
     }
 }
