71e5ce7dcbc47d4d6c1aacc3c5e4a1e9356e82ac
Jingxi <jing.xi@hotmail.com>
2020-08-09 03:37:09 +0000

    ..

--- Program.cs
+++ Program.cs
@@ -7,19 +7,19 @@ namespace sc_presure
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-  
- IPMI_Access.Foreloop();    
+            //IPMI_Access.Foreloop();    
+         Console.WriteLine(   Ex());
  //IPMI_Access.Foreloop();
  }
           static string Ex()
     {
 
-string command = "write your command here";
+string command = "ifconfig";//"write your command here";
 string result = "";
 using (System.Diagnostics.Process proc = new System.Diagnostics.Process())
 {
     proc.StartInfo.FileName = "/bin/bash";
-    proc.StartInfo.Arguments = "-c \" " + command + " \"";
+    proc.StartInfo.Arguments = "-c \" " + command + @" -a | grep -i  'inet ' ";
     proc.StartInfo.UseShellExecute = false;
     proc.StartInfo.RedirectStandardOutput = true;
     proc.StartInfo.RedirectStandardError = true;
