f0ed5377648956002fa9977242481d4e9c274e11
jingxi <jing.xi@hotmail.com>
2020-08-25 01:46:32 +0000

    ..

--- Program.cs
+++ Program.cs
@@ -1,78 +1,89 @@
 using System;
 using System.Threading;
 
-namespace sc_presure 
-{
-    class Program {
-        static void Main (string[] args) {
-            Console.WriteLine ("Hello World!");
-            //IPMI_Access.Foreloop();
-            //  Console.WriteLine (LSEX ());
-            //IPMI_Access.Foreloop();
-            for (int i = 0; i < 5; i++) {
-                Console.WriteLine (common_cmd (" ls"));
-                Thread.Sleep (2000);
-                Console.WriteLine (common_cmd (" ifconfig"));
-            }
-        }
-        // static string Ex ()
-        //  {
+namespace sc_presure {
+   class Program {
+      static void Main (string[] args) {
+         Te ();
 
-        //     string command = "ifconfig"; //"write your command here";
-        //     string result = "";
-        //     using (System.Diagnostics.Process proc = new System.Diagnostics.Process ()) {
-        //         proc.StartInfo.FileName = "/bin/bash";
-        //         proc.StartInfo.Arguments = "-c \" " + command + @" -a | grep -i  'inet ' ";
-        //         proc.StartInfo.UseShellExecute = false;
-        //         proc.StartInfo.RedirectStandardOutput = true;
-        //         proc.StartInfo.RedirectStandardError = true;
-        //         proc.Start ();
+      }
+      static void Te () {
+         Console.WriteLine ("Hello World!");
+         //IPMI_Access.Foreloop();
+         //  Console.WriteLine (LSEX ());
+         //IPMI_Access.Foreloop();
+         for (int i = 0; i < 1; i++) {
+            //  Console.WriteLine (common_cmd ("-c ls","/bin/bash"));
+            //  Thread.Sleep (300);
+            //  Console.WriteLine("----------------");
+            //  Console.WriteLine (common_cmd ("-c ifconfig","/bin/bash"));
+            //  Thread.Sleep (300);
+            Console.WriteLine ("----------------");
+            Console.WriteLine (common_cmd ("-c  lan print 1", "/bin/ipmitool"));
+            Thread.Sleep (300);
+            Console.WriteLine ("----------------");
+            Console.WriteLine (common_cmd ("-c  mc info", "/bin/ipmitool"));
+            Thread.Sleep (300);
+            Console.WriteLine ("----------------");
+            Console.WriteLine (common_cmd ("-c  fru ", "/bin/ipmitool"));
+            Thread.Sleep (300);
+            Console.WriteLine ("----------------");
+            Console.WriteLine (common_cmd ("-c  dcmi power reading ", "/bin/ipmitool"));
+            Thread.Sleep (300);
+            Console.WriteLine ("----------------");
+            Console.WriteLine (common_cmd ("-c  sdr ", "/bin/ipmitool"));
+            Thread.Sleep (300);
+            Console.WriteLine ("----------------");
+            Console.WriteLine (common_cmd ("  -t BIOS ", "/sbin/dmidecode"));
+            Thread.Sleep (300);
+            Console.WriteLine ("----------------");
+            Console.WriteLine (common_cmd ("  -t system ", "/sbin/dmidecode"));
+            Thread.Sleep (300);
+            Console.WriteLine ("----------------");
+            Console.WriteLine (common_cmd ("  -t baseboard ", "/sbin/dmidecode"));
+            Thread.Sleep (300);
+            Console.WriteLine ("----------------");
+            Console.WriteLine (common_cmd ("  -t processor ", "/sbin/dmidecode"));
+            Thread.Sleep (300);
+            Console.WriteLine ("----------------");
+            Console.WriteLine (common_cmd ("  -t memory ", "/sbin/dmidecode"));
+            Thread.Sleep (300);
+            Console.WriteLine ("----------------");
+            Console.WriteLine (common_cmd ("  -t slot ", "/sbin/dmidecode"));
+            Thread.Sleep (300);
+            Console.WriteLine ("----------------");
+            //    Console.WriteLine (common_cmd ("  |grep -i power ","/sbin/dmidecode"));
 
-        //         result += proc.StandardOutput.ReadToEnd ();
-        //         result += proc.StandardError.ReadToEnd ();
+         }
+      }
+  
+      static string common_cmd (string cmd, string filename) {
 
-        //         proc.WaitForExit ();
-        //     }
-        //     return result;
-        // }
-        // static string LSEX () {
+         string command = cmd; //"write your command here";
+         string filenamel = filename;
+         string result = "";
+         using (System.Diagnostics.Process proc = new System.Diagnostics.Process ()) {
+            proc.StartInfo.FileName = filenamel; //"/bin/bash";
+            proc.StartInfo.Arguments = "  " + command; //"-c  " + command;// + @" -a | grep -i  'inet ' ";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.Start ();
 
-        //     string command = "ls"; //"write your command here";
-        //     string result = "";
-        //     using (System.Diagnostics.Process proc = new System.Diagnostics.Process ()) {
-        //         proc.StartInfo.FileName = "/bin/bash";
-        //         proc.StartInfo.Arguments = "-c " + command; //"-c  " + command;// + @" -a | grep -i  'inet ' ";
-        //         proc.StartInfo.UseShellExecute = false;
-        //         proc.StartInfo.RedirectStandardOutput = true;
-        //         proc.StartInfo.RedirectStandardError = true;
-        //         proc.Start ();
+            result += proc.StandardOutput.ReadToEnd ();
+            result += proc.StandardError.ReadToEnd ();
 
-        //         result += proc.StandardOutput.ReadToEnd ();
-        //         result += proc.StandardError.ReadToEnd ();
+            proc.WaitForExit ();
+         }
+         return result;
+      }
 
-        //         proc.WaitForExit ();
-        //     }
-        //     return result;
-        // }
-        static string common_cmd (string cmd) {
-
-            string command = cmd; //"write your command here";
-            string result = "";
-            using (System.Diagnostics.Process proc = new System.Diagnostics.Process ()) {
-                proc.StartInfo.FileName = "/bin/bash";
-                proc.StartInfo.Arguments = "-c " + command; //"-c  " + command;// + @" -a | grep -i  'inet ' ";
-                proc.StartInfo.UseShellExecute = false;
-                proc.StartInfo.RedirectStandardOutput = true;
-                proc.StartInfo.RedirectStandardError = true;
-                proc.Start ();
-
-                result += proc.StandardOutput.ReadToEnd ();
-                result += proc.StandardError.ReadToEnd ();
-
-                proc.WaitForExit ();
-            }
-            return result;
-        }
-
-    }
+   }
+   public class Distor{
+      public static List<string,string> RowRecord(string raw_rec){
+        var sslist =new List<string,string>();
+        
+         
+      }
+   }
 }
